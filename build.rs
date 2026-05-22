a1fd301b740e503222ce3ef318dba2bf909b70d5
vlanl <76094587@qq.com>
2026-05-22 22:01:23 +0800

    Initial commit: SecureDesk hbb_common fork

---
+++ build.rs
@@ -0,0 +1,32 @@
+use std::env;
+
+fn set_env(key: &str) {
+    if let Some(val_os) = env::var_os(key) {
+        if let Some(val) = val_os.to_str() {
+            println!("cargo:rustc-env={}={}", key, val);
+        }
+    }
+}
+
+fn main() {
+    // 注入环境变量（用于自定义客户端编译）
+    set_env("APP_NAME");
+    set_env("RENDEZVOUS_SERVER");
+    set_env("RELAY_SERVER");
+    set_env("API_SERVER");
+    set_env("RS_PUB_KEY");
+    set_env("DEFAULT_PASSWORD");
+
+    let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
+
+    std::fs::create_dir_all(&out_dir).unwrap();
+
+    protobuf_codegen::Codegen::new()
+        .pure()
+        .out_dir(out_dir)
+        .inputs(["protos/rendezvous.proto", "protos/message.proto"])
+        .include("protos")
+        .customize(protobuf_codegen::Customize::default().tokio_bytes(true))
+        .run()
+        .expect("Codegen failed.");
+}
